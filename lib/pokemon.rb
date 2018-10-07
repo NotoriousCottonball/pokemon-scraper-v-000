203df8f7f852d6fdeeec50b8b82b1326eaabc6e1
George  Pianka <gpianka1@gmail.com>
2018-10-07 05:28:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,8 +14,7 @@ attr_accessor :id, :name, :type, :db, :hp
   end
   def self.find(id, db)
     pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    pokemon = self.new ({id: id, name: pokemon_from_db[1], type: pokemon_from_db[2], hp: pokemon_from_db[3], db: db})
-    pokemon
+    self.new ({id: id, name: pokemon_from_db[1], type: pokemon_from_db[2], hp: pokemon_from_db[3], db: db})
   end
    
   def alter_hp(new_hp, db)
