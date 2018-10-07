5a3a3cf5f69ea2119682e5093a89999708715f7c
George  Pianka <gpianka1@gmail.com>
2018-10-07 02:32:16 +0000

    automatically backed up by learn

--- bin/environment.rb
+++ bin/environment.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 Bundler.require
 
 require_relative "../lib/pokemon"
-require_relative "../lib/scraper"
+require_relative "scraper"
 require_relative "./sql_runner"
 
 @db = SQLite3::Database.new('db/pokemon.db')
